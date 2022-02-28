3827bb562dc0bbc6b9595cd442aa48c198dda003
RC2PC2 <petriccarcc@gmail.com>
2022-02-28 10:14:16 +0100

    js-mail exercise

---
+++ js-mail-dadi-livecoding/js/main.js
@@ -0,0 +1,44 @@
+/**
+ *
+ *  **Mail**
+    Chiedi all’utente la sua email,
+    controlla che sia nella lista di chi può accedere,
+    stampa un messaggio appropriato sull’esito del controllo.
+
+    *Gioco dei dadi*
+    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    Stabilire il vincitore, in base a chi fa il punteggio più alto.
+ *
+ */
+
+//  Database fittizio che contiene le mail registrate
+const emailList = [
+   'riccardo@gmail.com',
+   'viviana@gmail.com',
+   'massimo@gmail.com',
+   'manuel@gmail.com',
+   'arianna@gmail.com',
+   'gianfranco@gmail.com'
+];
+
+// chiediamo all'utente di inserire il suo indirizzo email
+const userEmail = prompt('Insert your email').toLowerCase().trim();
+
+// creo una variabile "flag" che assegnerò a true ==> troverò userMail in emailList
+let isRegistered = false;
+
+// creo un ciclo for per tutta la lunghezza della nostra lista emailList
+for ( let i = 0 ; i < emailList.length ; i++ ){
+   if( emailList[i] == userEmail ){
+      isRegistered = true;
+   }
+}
+
+// stampo un messaggio in console che mi dice se la mail è di un utente registrato oppure no
+if ( isRegistered == true ){  // potrei scrivere la condizione anche come ( isRegistered )
+   console.log('l\'utente è registrato');
+} else {
+   console.log('l\'utente non è registrato');
+}
+
+
