ea269857b9997deb1636d7285a096e9636902666
RC2PC2 <petriccarcc@gmail.com>
2022-02-28 10:20:49 +0100

    js-dices livecoding

--- js-mail-dadi-livecoding/js/main.js
+++ js-mail-dadi-livecoding/js/main.js
@@ -11,34 +11,54 @@
  *
  */
 
-//  Database fittizio che contiene le mail registrate
-const emailList = [
-   'riccardo@gmail.com',
-   'viviana@gmail.com',
-   'massimo@gmail.com',
-   'manuel@gmail.com',
-   'arianna@gmail.com',
-   'gianfranco@gmail.com'
-];
+// //  Database fittizio che contiene le mail registrate
+// const emailList = [
+//    'riccardo@gmail.com',
+//    'viviana@gmail.com',
+//    'massimo@gmail.com',
+//    'manuel@gmail.com',
+//    'arianna@gmail.com',
+//    'gianfranco@gmail.com'
+// ];
 
-// chiediamo all'utente di inserire il suo indirizzo email
-const userEmail = prompt('Insert your email').toLowerCase().trim();
+// // chiediamo all'utente di inserire il suo indirizzo email
+// const userEmail = prompt('Insert your email').toLowerCase().trim();
 
-// creo una variabile "flag" che assegnerò a true ==> troverò userMail in emailList
-let isRegistered = false;
+// // creo una variabile "flag" che assegnerò a true ==> troverò userMail in emailList
+// let isRegistered = false;
 
-// creo un ciclo for per tutta la lunghezza della nostra lista emailList
-for ( let i = 0 ; i < emailList.length ; i++ ){
-   if( emailList[i] == userEmail ){
-      isRegistered = true;
-   }
-}
+// // creo un ciclo for per tutta la lunghezza della nostra lista emailList
+// for ( let i = 0 ; i < emailList.length ; i++ ){
+//    if( emailList[i] == userEmail ){
+//       isRegistered = true;
+//    }
+// }
 
-// stampo un messaggio in console che mi dice se la mail è di un utente registrato oppure no
-if ( isRegistered == true ){  // potrei scrivere la condizione anche come ( isRegistered )
-   console.log('l\'utente è registrato');
+// // stampo un messaggio in console che mi dice se la mail è di un utente registrato oppure no
+// if ( isRegistered == true ){  // potrei scrivere la condizione anche come ( isRegistered )
+//    console.log('l\'utente è registrato');
+// } else {
+//    console.log('l\'utente non è registrato');
+// }
+
+
+// genero un numero randomico da 1 a 6 inclusi.
+const playerNumber = Math.floor(Math.random() * 6) + 1;
+console.log('Il tuo valore è '+playerNumber);
+
+// genero un numero randomico da 1 a 6 inclusi.
+const aINumber = Math.floor(Math.random() * 6) + 1;
+console.log('Il mio valore è '+aINumber);
+
+// verifico le varie condizioni
+if ( playerNumber > aINumber){
+   console.log('Hai vinto!');
+} else if ( aINumber > playerNumber){
+   console.log('Ha vinto l\'AI!');
 } else {
-   console.log('l\'utente non è registrato');
+   console.log('Siamo pari!');
 }
+
+
 
 
