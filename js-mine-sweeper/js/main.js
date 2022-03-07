3f8d1843205fe22dc7377908dd72e96d086e2a99
RC2PC2 <petriccarcc@gmail.com>
2022-03-07 09:51:01 +0100

    js-mine-sweeper basic generation

--- js-mine-sweeper/js/main.js
+++ js-mine-sweeper/js/main.js
@@ -1,108 +1,93 @@
-
-// Arrow function che restituisce un div di square sotto forma di elemento del DOM
-const createGridSquare = () => {
-    const currentElement = document.createElement('div');
-    // inseriamo tutto il necessario per ogni square
-    currentElement.classList.add('square');
-    return currentElement;
-}
-
-// Arrow function che restituisce un div di square sotto forma di elemento del DOM
-const createGridSquareWithNumber = (number) => {
-    const currentElement = document.createElement('div');
-    // inseriamo tutto il necessario per ogni square
-    currentElement.classList.add('square');
-
-    if( number % 2 === 0){   //se il numero è pari
-        currentElement.classList.add('square-even');
-    } else {    // se invece è dispari
-        currentElement.classList.add('square-odd');
-    }
-
-    //  aggiungo lo span
-    currentElement.innerHTML = `
-    <span class="square-number">${number}</span>`
-
-    return currentElement;
-}
-
-
-// prendo l'elemento principale
-const gridElement = document.getElementById('grid');
-
-// creo la lista dei numeri per vedere quali numeri ho già inserito, ovvero la nostra "blacklist"
-const numBlacklist = [];
-
-// ciclo for per 64 volte (visto che voglio 64 numeri)
-for (let i = 0 ; i < 64 ; i++){
-
-    // mi genero un nuovo numero randomico assente nella blacklist
-    let newValidInteger = generateUniqueRandomNumber( numBlacklist, 1, 64);
-
-    // lo inserisco in blacklist
-    numBlacklist.push(newValidInteger);
-    // stampo in console
-    console.warn(newValidInteger);
-
-    // creo un singolo quadrato
-    const currentSquare = createGridSquareWithNumber(newValidInteger);
-
-    // aggiungo l'evento di click e i suoi effetti
-    currentSquare.addEventListener('click', function() {
-        // | this = currentSquare;
-        this.classList.toggle('clicked');
-    })
-
-    // infine lo aggiungo al parent
-    gridElement.appendChild(currentSquare);
-}
-
+// Sia Cosmo che Salvatore hanno avuto difficoltà per il numero di generazioni successive al primo
 
 /**
- *  ******************************* FUNCTIONS *************************************
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco di quadrati, in cui ogni cella contiene un numero tra quelli compresi in un range:
+
+    con difficoltà 1 => tra 1 e 100
+    con difficoltà 2 => tra 1 e 81
+    con difficoltà 3 => tra 1 e 49
+
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
  */
 
-/**
- * Function that returns a random numer between two values (both included).
- *
- * @param {*} minimumValue The minimum value of the random number to be generated
- * @param {*} maximumValue The maximum value of the random number to be generated
- * @returns The randon generated number.
- */
-function randomInteger(minimumValue, maximumValue){
-    if ( isNaN(parseInt(minimumValue)) || isNaN(parseInt(maximumValue)) ){
-        console.error('randomInteger(min, max) needs two numbers as argument');
+// const playButton = document.getElementById('play');
+document.getElementById('play').addEventListener( "click",
+    function(){
+        createNewGame();
+    }
+)
+
+// funzione per generare una nuova partita SOVRASCRIVENDO la precedente
+function createNewGame(){
+
+    // Reset generale per il contenitore interno della grid
+    document.querySelector('#grid').innerHTML = "";
+
+    // prima di tutto recupero il livello selezionato dall'utente
+    // stabilisco le condizioni date dal livello selezionato
+    const level = parseInt( document.getElementById('level').value );
+    console.log(level);
+
+    // GENERAZIONE DELLA GRIGLIA
+    // stabilire il numero delle caselle che devono apparire:
+        //  prendere il valore in base alla difficoltà
+        // due valori : numero di celle per riga e numero di celle totali
+
+    let cellsPerRow;
+    let cellsNumber;
+
+    switch (level ) {
+        case 1:
+        default:
+            cellsNumber = 100;
+            // cellsPerRow = 10;
+            break;
+        case 2:
+            cellsNumber = 81;
+            // cellsPerRow = 9;
+            break;
+        case 3:
+            cellsNumber = 49;
+            // cellsPerRow = 7;
+            break;
     }
 
-    return ( Math.floor(Math.random() * ((maximumValue + 1) - minimumValue) + minimumValue));
+    // if ( level == 1 ){
+    //     cellsNumber = 100;
+    // } else if (level == 2 ){
+    //     cellsNumber = 81;
+    // }  else if (level == 3 ){
+    //     cellsNumber = 49;
+    // }
+
+    //  cellsPerRow => radice quadrata di cellsNumber
+    cellsPerRow = Math.sqrt(cellsNumber);
+
+    //  prendo il parent
+    //  ciclo per tutti gli elementi che voglio creare
+    //  aggiungo un event listener per far colorare il bottone
+    //  li inserisco come elementi figli del nostro parent selezionato
+
+    for ( let i = 1; i <= cellsNumber; i++){
+        const cell = createSquare(i, cellsPerRow);
+        cell.addEventListener('click', function(){
+            this.classList.add('clicked');
+        });
+        document.querySelector('#grid').appendChild(cell);
+    }
+
 }
 
-/**
- * Function that generates a random number between two included values, which is not already present in the given blacklist.
- *
- * @param {*} numsBlacklist The blacklist to check.
- * @param {*} min The minimum value of the random generated integer.
- * @param {*} max The maximum value of the random generated integer.
- * @returns A random generated integer which is not present in the blacklist.
- */
-function generateUniqueRandomNumber( numsBlacklist, min, max){
-    // mi creo una variabile inizializzata a false, che mi controlla se ho generato un numero
-    // valido oppure no
-    let check = false;
-    let randomInt;
+// genera il singolo quadrato
+function createSquare(number, cellsPerRow){
+    let cell = document.createElement('div');
+    // cell.className = ('square');
+    cell.classList.add('square');
 
-    // creo un ciclo che continua finché non ho trovato un numero valido (assente in blacklist)
-    while ( !check ){
-        //  genero randomicamente un numero intero tra il min e il max passati come argomenti
-        randomInt  = randomInteger(min, max);
-        // se il numero non è presente nella blacklist allora
-        if ( !numsBlacklist.includes(randomInt)  ){
-            // informo il resto della funzione che il numero è stato trovato ed è valido
-            // ==> esco dal ciclo while
-            check = true;
-        }
-    }
+    cell.style.width = `calc(100% / ${cellsPerRow})`
+    cell.style.height = cell.style.width;
 
-    // restituisco il numero valido che ho trovato
-    return randomInt;
+    cell.innerHTML = `<span>${number}</span>`;
+
+    return cell;
 }
