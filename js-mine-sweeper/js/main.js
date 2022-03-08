10666529e7ac7930132e67863d718de601a91191
RC2PC2 <petriccarcc@gmail.com>
2022-03-08 10:36:52 +0100

    js-minesweeper livecoding + bonuses

--- js-mine-sweeper/js/main.js
+++ js-mine-sweeper/js/main.js
@@ -23,6 +23,9 @@ function createNewGame(){
     // Reset generale per il contenitore interno della grid
     document.querySelector('#grid').innerHTML = "";
 
+    // domanda, il gioco è finito?
+    let isGameOver = false;
+
     // prima di tutto recupero il livello selezionato dall'utente
     // stabilisco le condizioni date dal livello selezionato
     const level = parseInt( document.getElementById('level').value );
@@ -35,6 +38,9 @@ function createNewGame(){
 
     let cellsPerRow;
     let cellsNumber;
+
+    let points = 0;
+    const NUMBER_OF_BOMBS = 16;
 
     switch (level ) {
         case 1:
@@ -63,31 +69,135 @@ function createNewGame(){
     //  cellsPerRow => radice quadrata di cellsNumber
     cellsPerRow = Math.sqrt(cellsNumber);
 
+    //  ? genero la lista delle bombe casuali
+    const bombs = generateBombList(NUMBER_OF_BOMBS, cellsNumber);
+    console.log(bombs);
+
     //  prendo il parent
     //  ciclo per tutti gli elementi che voglio creare
     //  aggiungo un event listener per far colorare il bottone
     //  li inserisco come elementi figli del nostro parent selezionato
 
+
     for ( let i = 1; i <= cellsNumber; i++){
         const cell = createSquare(i, cellsPerRow);
+
         cell.addEventListener('click', function(){
-            this.classList.add('clicked');
+            // se il gioco non è finito allora
+            if (!isGameOver){
+                if (!bombs.includes(i)){
+                    this.classList.add('clicked'); // this === cell
+                    points++;   //points = points + 1;
+                    writeInElementById('points', `Il tuo punteggio è: ${points}`);
+                    //  aggiungi un punto
+                } else {
+                    this.classList.add('clicked-bomb'); // this === cell
+                    writeInElementById('points', `Mi dispiace, hai perso, il tuo punteggio è: ${points}`);
+                    checkAndAddClass('grid', bombs, 'clicked-bomb');
+                    isGameOver = true;
+                }
+            }
         });
         document.querySelector('#grid').appendChild(cell);
     }
-
 }
 
-// genera il singolo quadrato
+// genera il singolo quadrato e me lo restituisce
 function createSquare(number, cellsPerRow){
     let cell = document.createElement('div');
     // cell.className = ('square');
     cell.classList.add('square');
-
     cell.style.width = `calc(100% / ${cellsPerRow})`
     cell.style.height = cell.style.width;
-
     cell.innerHTML = `<span>${number}</span>`;
 
     return cell;
+}
+
+
+function generateBombList(bombs, numberOfCells){
+    // dati il numero di bombe da generare "bombs"
+    // ed il numero di celle disponibili (generate) "numberOfCells"
+    // inizializzo a vuoto un nuovo array
+    const bombList = [];
+    // per ogni bomba
+    for(i = 0 ; i < bombs ; i++){
+        // ne genero una nuova che non sia già presente e la aggiungo alla lista delle bombe
+        bombList.push(generateUniqueRandomNumber(bombList, 1, numberOfCells));
+    }
+    // restituisco la lista delle bombe
+    return bombList;
+}
+
+
+/**
+ * Function that generates a random number between two included values, which is not already present in the given blacklist.
+ *
+ * @param {*} numsBlacklist The blacklist to check.
+ * @param {*} min The minimum value of the random generated integer.
+ * @param {*} max The maximum value of the random generated integer.
+ * @returns A random generated integer which is not present in the blacklist.
+ */
+function generateUniqueRandomNumber( numsBlacklist, min, max){
+    // mi creo una variabile inizializzata a false, che mi controlla se ho generato un numero
+    // valido oppure no
+    let check = false;
+    let randomInt;
+
+    // creo un ciclo che continua finché non ho trovato un numero valido (assente in blacklist)
+    while ( !check ){
+        //  genero randomicamente un numero intero tra il min e il max passati come argomenti
+        randomInt  = Math.floor(Math.random() * ((max + 1) - min) + min);
+        // se il numero non è presente nella blacklist allora
+        if ( !numsBlacklist.includes(randomInt)  ){
+            // informo il resto della funzione che il numero è stato trovato ed è valido
+            // ==> esco dal ciclo while
+            check = true;
+        }
+    }
+
+    // restituisco il numero valido che ho trovato
+    return randomInt;
+}
+
+// Funzione che scrive in un elemento del dom preso da id "elementId" sovrascrivendo ciò che è presente
+function writeInElementById(elementId, stringToWrite){
+    document.getElementById(elementId).innerHTML = stringToWrite;
+}
+
+/**
+ * Alessio, Vittorio, Franesco, Manuel, Salvatore difficoltà con la realizzazione dell'interruzione del gioco "game over" CHECK
+ *
+ * Francesco C. funzione generare bombe uniche (anche l'altro Francesco all'inizio)
+ *
+ * Marco & Salvatore : click vale all'infinito per i punti      CHECK
+*               1)   Fra G. event listener  --> usando (){}
+ *              2)      Alessio => removeEventListener()
+ *              3)      Matteo T. pointer events none (lato CSS)
+ *
+ *  Airowl : implementazione iniziale scoperta di tutte le bombe        CHECK
+ */
+
+function checkAndAddClass( parentElementId , bombList, classToAdd){
+    const squares = document.getElementById(parentElementId).children;
+
+    // § Don Matteo alternative
+    // per ogni quadrato presente
+    for (let i= 0 ; i < squares.length ; i++ ){
+        // se è anche una bomba
+        if ( bombList.includes(parseInt(squares[i].firstChild.innerHTML)) ){
+            // la faccio esplodere
+            squares[i].classList.add(classToAdd);
+        }
+    }
+
+    // | Alessio alternative
+    //  // per ogni quadrato presente
+    // for (let i= 0 ; i < bombList.length ; i++ ){
+    //     // se è anche una bomba
+    //     if ( bombList.includes(parseInt(squares[i].firstChild.innerHTML)) ){
+    //         // la faccio esplodere
+    //         squares[bombList[i]].classList.add(classToAdd);
+    //     }
+    // }
 }
