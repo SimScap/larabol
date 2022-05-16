251e60edb227fd0386f333a48f0fcf79fc4cc6de
RC2PC2 <petriccarcc@gmail.com>
2022-05-16 11:55:30 +0200

    initi laravel-social-try

---
+++ laravel-social-try/resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <span class="navbar-text">
+                Footer di prova inserito come partial
+            </span>
+        </div>
+    </nav>
+</footer>
---
+++ laravel-social-try/resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div id="index-header">
+        <!-- As a link -->
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Homepage</a>
+            </div>
+        </nav>
+    </div>
+</header>
